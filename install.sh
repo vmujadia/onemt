8b9d45b2339f1780c48ca15187e2de7bedc9eab4
vandan mujadia <vmujadia@gmail.com>
2024-07-27 13:13:55 +0530

    working inferencing

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+virtualenv -p python3.8 venv
+
+source venv/bin/activate
+pip3 install --upgrade pip==24.0.0
+
+pip3 install -r requirements.txt
+
+git clone https://github.com/pytorch/fairseq
+cd fairseq
+python3 -m pip install .
+
+cd ..
